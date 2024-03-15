66e2e586a31f30deabd0228597727fd3ca1f034a
Debora Ratto <drattodourado@gmail.com>
2024-03-15 10:20:47 -0300

    extract funcionando como modulo

---
+++ ETL/main.py
@@ -0,0 +1,5 @@
+from pipeline import extract
+
+lista_de_data_frame= extract.extract_from_excel("data/input")
+
+print(lista_de_data_frame)
---
+++ ETL/pipeline/extract.py
@@ -0,0 +1,26 @@
+import os
+import glob
+import pandas as pd
+
+"""
+    função para ler os arquivos de uma pasta data/input e retornar uma lista de dataframes.
+    
+    args: input_path(str): caminho da pasta com os arquivos
+    
+    return: lista de dataframes
+"""
+
+path= "data/input"
+
+def extract_from_excel(path: str) -> list[pd.DataFrame]:
+    all_files= glob.glob(os.path.join(path, "*.xlsx"))
+    
+    data_frame_list= []
+    for file in all_files:
+        data_frame_list.append(pd.read_excel(file))
+        
+    return data_frame_list
+
+if __name__ == "__main__":
+    data_frame_list= extract_from_excel(path)
+    print(data_frame_list)
